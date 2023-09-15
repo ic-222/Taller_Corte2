7eb429bbe6b9a17cdbd16077de8e751aaed32ef5
David gomez <davenrique15gomezaconcha@gmail.com>
2023-09-15 14:42:40 -0500

    Acción de agregar tarea

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import pyodbc
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
@@ -24,6 +25,18 @@ def mostrar_tasks():
     return render_template('mostrar_tareas.html', tareas=tareas)
 
 ## AGREGAR ACCIONES
+## Agregar tarea
+
+@app.route('/agregar_tarea', methods=['POST'])
+def agregar_tarea():
+    if request.method == 'POST':
+        descripcion = request.form['descripcion']
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO Tareas (descripcion, estado) VALUES (?, ?)", (descripcion, 'No Completado'))
+        conn.commit()
+        conn.close()
+    return redirect(url_for('mostrar_tareas'))
 
 
 
